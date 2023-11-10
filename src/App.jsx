37a0d9c2351de42e0bb5a4c79050192137ef6ee0
Ismoil2009 <osnovavbs092009@gmail.com>
2023-11-10 22:27:55 +0500

    unsuccessful finish

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./App.css";
+
+import {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  Homelayout,
+  Landing,
+  Login,
+  Orders,
+  Products,
+  Register,
+  SingleProduct,
+} from "./pages";
+
+
+function App() {
+  return (
+    <>
+      <h1>d</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Homelayout.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Homelayout() {
+  return (
+    <div>Homelayout</div>
+  )
+}
